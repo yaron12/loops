f3061ee001d299699869e74a6ac78595807492a6
yaronbensimon1@gmail.com <026535898>
2016-08-24 20:17:27 +0300

    loop

---
+++ loop.js
@@ -0,0 +1,46 @@
+//        exercise 1
+for (var i = 120; i <= 140; i++){
+ console.log(i);
+}
+
+//       exercise 2
+var star = '';
+    for(var i=0; i<5; i++){
+        star += '*';
+        console.log(star);
+    }
+    
+
+//       exercise 3
+
+var person= [
+   yaron = {
+    "name":"yaron",
+     "eyecolor":"brown",
+      "height": 187
+    },
+   eli =  {
+    "name":"eli",
+     "eyecolor":"black",
+      "height": 185
+    },
+  miki =  {
+    "name":"miki",
+     "eyecolor":"green",
+      "height": 183
+    }
+];
+
+for (var i in person) {
+  console.log("height" + " " + "is" + " " + person[i].height);
+}
+
+//       exercise  4
+
+for (var i in person) {
+  person[i].height=175;
+}
+
+for (var i in person) {
+  console.log("height" + " " + "is" + " " + person[i].height);
+}
